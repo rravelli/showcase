bc96cd54cb9b58aa86a431d20ca940858ff9f307
rravelli <remi.ravelli@gmail.com>
2023-11-22 19:41:02 +0100

    react template

---
+++ src/data/user.js
@@ -0,0 +1,47 @@
+const INFO = {
+	main: {
+		title: "Remi Ravelli's portfolio",
+		name: "Rémi R.",
+		email: "remi.ravelli@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		// twitter: "https://twitter.com/",
+		github: "https://github.com/rravelli",
+		linkedin: "https://www.linkedin.com/in/r%C3%A9mi-ravelli/",
+		// instagram: "https://instagram.com/",
+		// stackoverflow: "https://stackoverflow.com/",
+		// facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer, software engineer.",
+		description:
+			"I'm a fullstack developer with a preference for backend development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. My work is not centered on web development, indeed I had work on projects related to software engineering, mobile development. To sum it all, I like to have my hand on everything and learn new things. I look forward to work on my next project with you.",
+	},
+
+	about: {
+		title: "I’m Rémi R. I live in France, where I studied computer science.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "Nothing here. For now...",
+		description: ""
+	},
+
+	projects: [
+		{
+			title: "Nantral Platform",
+			description:
+				"The student website for Centrale Nantes. Frontend with ReactJS and backend with Django",
+			logo: "https://nantral-platform.fr/static/img/logo/scalable/logo.svg",
+			linkText: "View Project",
+			link: "https://github.com/3cn-ecn/nantralPlatform",
+		},
+	],
+};
+
+export default INFO;
